67d9bd7e50eb7b6c52427a54b71c378bf3790321
Kevin Hawkins <khawkins@salesforce.com>
2012-08-05 14:12:33 -0700

    Adding expanded packages to source, script to build archive

---
+++ CloudTunes-metadata/make_package.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ \( -z "$1" \) -o \( "$1" != "CloudTunes-force" -a "$1" != "CloudTunes-database" \) ]
+then
+    echo "Usage: $0 CloudTunes-force | CloudTunes-database"
+    exit 1
+fi
+
+archive_dir=$1
+archive_name=$archive_dir.zip
+cwd=$( dirname "${BASH_SOURCE[0]}" )
+
+cd $cwd
+echo "Archiving $archive_dir to $archive_name"
+ditto -k -c --norsrc $archive_dir $archive_name
+
